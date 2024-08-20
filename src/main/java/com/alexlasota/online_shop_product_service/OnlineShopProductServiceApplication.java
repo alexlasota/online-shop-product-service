216532f77503daeffe7a2acd0f86e8cc74da3744
alexlasota <alexylasota@gmail.com>
2024-08-20 20:41:57 +0200

    unversioned files

---
+++ src/main/java/com/alexlasota/online_shop_product_service/OnlineShopProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.alexlasota.online_shop_product_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShopProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineShopProductServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alexlasota/online_shop_product_service/OnlineShopProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexlasota.online_shop_product_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShopProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
